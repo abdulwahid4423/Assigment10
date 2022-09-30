5d676a5a2db86f45aaa309bcb20ebd10a17bc03e
Abdul Wahid <111135216+abdulwahid4423@users.noreply.github.com>
2022-09-30 05:50:38 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+function circle(){
+    var cr = document.getElementById("circle")
+    cr.className = "circlebtu"
+}
+
+function squr(){
+    var cr = document.getElementById("circle")
+    cr.className = "squarebtu"
+}
+
+function tringle(){
+    var cr = document.getElementById("circle")
+    cr.className = "trgbtu"
+}
